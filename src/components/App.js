8c195070beb98c920eefd7bd4d2650a36da239db
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-07-20 20:12:58 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    // Function to fetch data from the API
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        setDogImage(data.message);
+      } catch (error) {
+        console.error('Error fetching dog image:', error);
+      }
+    };
+
+    // Call the function to fetch the data
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
